223e5b2c6fc7c3bd929f99859bb37e7384bdaca8
Carlos <Carlos@DESKTOP-H00EK4I>
2026-05-25 14:04:02 -0300

    feat: cria estrutura inicial da API de seguros

---
+++ src/main/java/com/carlos/api_seguros/ApiSegurosApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.api_seguros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSegurosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSegurosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/api_seguros/ApiSegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.api_seguros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSegurosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
